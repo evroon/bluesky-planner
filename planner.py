b1eaa28fbddd14ba76f7af9e2811e3ea78f915bf
Erik Vroon <erik.vroon22@gmail.com>
2020-02-26 14:28:01 +0100

    Add great circle points

--- planner.py
+++ planner.py
@@ -1,6 +1,8 @@
 """ BlueSky route planner plugin. """
     
 from bluesky import stack, navdb  #settings, traf, sim, scr, tools
+import numpy as np
+import pyproj
 
 def init_plugin():
     config = {
@@ -19,6 +21,7 @@ def init_plugin():
             plan,
             'Plan a route from [origin] to [destination].']
     }
+
     return config, stackfunctions
 
 
@@ -31,5 +34,33 @@ def preupdate():
 def reset():
     pass
 
+# Source: https://gis.stackexchange.com/questions/47/what-tools-in-python-are-available-for-doing-great-circle-distance-line-creati
+def create_great_circle(startlon, startlat, endlong, endlat):
+    g = pyproj.Geod(ellps='WGS84')
+    (az12, az21, dist) = g.inv(startlon, startlat, endlong, endlat)
+    lonlats = g.npts(startlon, startlat, endlong, endlat, 1 + int(dist / 10000))
+
+    # npts doesn't include start/end points, so prepend/append them
+    # lonlats.insert(0, (startlon, startlat))
+    # lonlats.append((endlong, endlat))
+    return lonlats
+
 def plan(origin="EHAM", destination="VHHH"):
+    origin_index = np.where(np.array(navdb.aptid) == origin)
+    destination_index = np.where(np.array(navdb.aptid) == destination)
+
+    if len(origin_index) < 1:
+        return False, 'Could not find origin'
+
+    if len(destination_index) < 1:
+        return False, 'Could not find destination'
+
+    origin_lon, origin_lat = navdb.aptlon[origin_index], navdb.aptlat[origin_index]
+    destination_lon, destination_lat = navdb.aptlon[destination_index], navdb.aptlat[destination_index]
+
+    points = create_great_circle(origin_lon, origin_lat, destination_lon, destination_lat)
+
+    for i, p in enumerate(points):
+        stack.stack('CIRCLE ' + ','.join(['circle_point_' + str(i), str(p[1]), str(p[0]), '10']))
+
     return True, 'Planning a route from {} to {}'.format(origin, destination)
